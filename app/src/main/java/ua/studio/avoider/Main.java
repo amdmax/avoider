fe7a65482ada239aa619fffa3600144cb1a72445
pdadayan <paruir.dadayan@logianalytics.com>
2015-06-08 22:05:02 +0300

    Map displayed

--- app/src/main/java/ua/studio/avoider/Main.java
+++ app/src/main/java/ua/studio/avoider/Main.java
@@ -1,18 +1,51 @@
 package ua.studio.avoider;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Main extends FragmentActivity {
-
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+public class Main extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SupportMapFragment fragmentById = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentMap);
+        fragmentById.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+        googleMap.setMyLocationEnabled(true);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        MapsInitializer.initialize(this);
+
+        // Check if we were successful in obtaining the map.
+        if (googleMap != null) {
+            initLocationChangeListener(googleMap);
+        }
+    }
+
+    private void initLocationChangeListener(final GoogleMap googleMap) {
+        GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+                googleMap.addMarker(new MarkerOptions().position(loc).title("Marker"));
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+            }
+        };
+
+        googleMap.setOnMyLocationChangeListener(myLocationChangeListener);
     }
 }
