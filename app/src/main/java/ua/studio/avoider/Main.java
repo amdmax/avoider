47c76219b0c4a1b1315a60110ec8b216e3df0aeb
pdadayan <paruir.dadayan@logianalytics.com>
2015-06-09 21:05:50 +0300

    Added connection validation

--- app/src/main/java/ua/studio/avoider/Main.java
+++ app/src/main/java/ua/studio/avoider/Main.java
@@ -1,8 +1,13 @@
 package ua.studio.avoider;
 
+import android.app.Activity;
 import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,15 +17,25 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Main extends FragmentActivity implements OnMapReadyCallback {
+public class Main extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
+
+    Button alramButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment fragmentById = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentMap);
-        fragmentById.getMapAsync(this);
+        alramButton = (Button) findViewById(R.id.alarmButton);
+        if (isConnectionAvailable()) {
+            SupportMapFragment fragmentById = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentMap);
+            fragmentById.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 
     @Override
@@ -47,5 +62,14 @@ public class Main extends FragmentActivity implements OnMapReadyCallback {
         };
 
         googleMap.setOnMyLocationChangeListener(myLocationChangeListener);
+    }
+
+    private boolean isConnectionAvailable() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
     }
 }
